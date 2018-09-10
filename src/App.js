56a51ba8f0825a2207c30b3f4a54acdb03e8ef04
Ebba Louise Toreld Fingarsen <elfingar@stud.ntnu.no>
2018-09-10 17:32:24 +0200

    lagd enkel responsivitet #9

--- src/App.js
+++ src/App.js
@@ -6,13 +6,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+          {/*<header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </header>*/}
+          <main>
+              <div id="nav">
+                  {/*nav bar */}
+                  <img src="https://itknowledgeexchange.techtarget.com/overheard/files/2016/08/hamburger-icon.png"></img>
+              </div>
+              <div class="container">
+                  {/* Placeholder bilde for nå, må hente media på annen måte senere.*/}
+                  <div class="item" id="picture">
+
+                  </div>
+                  <div class="item" id="text">
+                      {/*Her må vi hente ut json objekt med AJAX*/}
+                      <p> Dette er bare noe vakker placeholde tekst.</p>
+                      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae semper quam. Praesent lobortis tellus quis erat condimentum, a bibendum tortor volutpat.</p>
+                  </div>
+              </div>
+          </main>
       </div>
     );
   }
